ed81b61c24cbd7e381c89330aea8046346db4b13
Lucas Pinheiro <lucaslogos@gmail.com>
2023-11-23 16:58:08 -0300

    update

--- Program.cs
+++ Program.cs
@@ -20,6 +20,30 @@ void menuAplicacao()
     Console.WriteLine($"Digite 4 para exibir a média de uma banda");
     Console.WriteLine($"Digite 0 para sair");
 
+    Console.Write($"\nDigite a sua opção:");
+    string opcaoEcolhida = Console.ReadLine()!;
+    int opcaoEcolhidaNumerica = int.Parse(opcaoEcolhida);
+    switch (opcaoEcolhidaNumerica)
+    {
+        case 1:
+            Console.WriteLine($"Opção escolhida {1}");
+            break;
+        case 2:
+            Console.WriteLine($"Opção escolhida {2}");
+            break;
+        case 3:
+            Console.WriteLine($"Opção escolhida {3}");
+            break;
+        case 4:
+            Console.WriteLine($"Opção escolhida {4}");
+            break;
+        case 0:
+            Console.WriteLine($"Opção escolhida {0}");
+            break;
+        default:
+            Console.WriteLine("Opção invalida!");
+            break;
+    };
 };
 mensagemDeBoasVindas();
 menuAplicacao();
